7cde3c920d01041ec6c6cc44bcbeb64e0a88b36d
Dourfyt <gamemastertv2016@gmail.com>
2024-04-03 11:08:31 +0300

    Обновление дизайна

--- golos/models.py
+++ golos/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import PIL
 
+from PIL import Image
+from imagekit.models.fields import ImageSpecField
+from imagekit.processors import ResizeToFit, Adjust,ResizeToFill
 class UserD(AbstractUser):
     is_voted = models.BooleanField(default=False)
     pass
@@ -11,6 +15,9 @@ class Candidat(models.Model):
     last_name = models.CharField(max_length=40)
     otchestvo = models.CharField(max_length=40)
     votes = models.PositiveIntegerField(default=0)
-
+    img = models.ImageField(upload_to='candidats/', null=True, blank=True)
+    photo_big = ImageSpecField([Adjust(contrast=1.2, sharpness=1.1),
+                                ResizeToFill(width=480,height=480)], source='img',
+                               format='JPEG', options={'quality': 90})
 
 # Create your models here.
